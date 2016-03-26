d70498adf427e1497d90485dc36cf7aa42d0f429
Jason Ziolo <jziolo@mymail.mines.edu>
2016-03-26 14:45:32 -0600

    commit

--- src/Projects2algorithms.java
+++ src/Projects2algorithms.java
@@ -7,26 +7,18 @@ public class Projects2algorithms {
 		
 	}
 	
-	// correct to initially call min_cost_path(true, 0, 0, 0, n, gi, empty array)
 	public double[] min_cost_path(double curr_gals, int curr_day, int n, double[] needed_gals, double[] curr_path) {
 		// first check possibility that we do not have enough to cover cost
-		boolean make_order = false;
 		if(needed_gals[curr_day + 1] > curr_gals) {
-			make_order = true;
-		}
-		if(make_order) {
 			// have to make an order
 			// order size varies by how much would be required for the ____ days ahead
 			// where we compare to find the best option recursively
 			// ex: ordering to last 1 day vs. ordering to last the next 2 days
-			double min_amount = needed_gals[curr_day + 1] - curr_gals;
-			double possible_amount = min_amount;
-			// first possibility: we order enough for the next day only:
-			double[] possibilities = new double[n];
-			possibilities[0] = possible_amount;
-			for(int i = curr_day; i <= n; i++) {
-				possible_amount += needed_gals[i];
-				possibilities[i] = possible_amount;
+			double amount = needed_gals[curr_day + 1] - curr_gals;
+			double[] possibilities = new double[n - curr_day];
+			for(int i = 0; i < n - curr_day; i++) {
+				possibilities[i] = amount;
+				amount += needed_gals[i + curr_day + 1];
 			}
 			// now try all possibilities
 			for(int i = 0; i < n; i++) {
