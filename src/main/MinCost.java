2c8891ff4dd088e43bfc848ce1cb7dee93b4c51f
Jason Ziolo <jziolo@mymail.mines.edu>
2016-03-29 19:38:50 -0600

    Finished first try at table generation

--- src/main/MinCost.java
+++ src/main/MinCost.java
@@ -15,30 +15,44 @@ public class MinCost {
 		this.n = n;
 	}
 	
-	public int[][] generate_table() {
-		int[][] out = new int[n][n];
-		for(int i = 1; i <= n; i++) {
-			for(int j = i; j <= n; j++) {
+	public TableCell[][] generate_table() {
+		TableCell[][] out = new TableCell[n][n];
+		for(int i = 0; i < n; i++) {
+			for(int j = i; j < n; j++) {
 				if(i == 0 && j == 0) {
-					out[i][j] = P;	// first element
+					out[i][j].setCost(P);	// first element
+					break;
 				}
 				else if(i == 0) {	// first row
-					out[i][j] = out[i][j-1] + (j-1)*C*g[j];
+					out[i][j].setCost(out[i][j-1].getCost() + (j-1)*C*g[j]);
+					break;
 				}
 				int[] option = new int[3];
-				option[0] = j*C*g[i]; // top
-				option[1] = option[0] + P; // top-left
-				option[2] = P; // left
-				if(i == j) {	// ignore the left option
-					int min = option[0];
-					if(min > option[1]) {
-						min = option[1];
-					}
-					out[i][j] = out[]
+				option[0] = out[i-1][j].getCost() + j*C*g[i]; // top
+				option[1] = out[i-1][j-1].getCost() + j*C*g[i] + P; // top-left
+				int min = option[0];
+				if(min > option[1]) {	// top-left
+					min = option[1];
+					out[i][j].setParent_dir(Direction.UP_LEFT);
 				}
+				else {
+					min = option[0]; //top
+					out[i][j].setParent_dir(Direction.UP);
+				}
+				if(i == j) {	// if there is no left neighbor, do not attempt option 2
+					out[i][j].setCost(min);
+					break;
+				}
+				option[2] = out[i][j-1].getCost() + P; // left
+				if(min > option[2]) {
+					min = option[2];
+					out[i][j].setParent_dir(Direction.LEFT);
+				}
+				out[i][j].setCost(min);
 			}
 		}
 		return out;
 	}
 	
+	//public get_path()
 }
